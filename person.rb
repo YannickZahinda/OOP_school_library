776c9c29fb8ed050d732a1a2f431bc720c0199cf
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-06-24 17:37:54 +0200

    add decoration namable

--- person.rb
+++ person.rb
@@ -21,3 +21,30 @@ class Person
     is_of_age? || @parent_permission
   end
 end
+
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameable.correct_name.length > 10
+      @nameable.correct_name[0..9]
+    else
+      @nameable.correct_name
+    end
+  end
+end
